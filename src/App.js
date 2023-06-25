1ed33016b5b2e7d6c20045b56e228d0c4cd54ebb
Rakesh Ratnam <rakeshratnam56@gmail.com>
2023-06-26 00:27:29 +0530

    Project Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Button from './components/Button.component';
-import Form from './components/Form.Component';
+import DisplayComponent from './components/Display.component';
+import FormComponent from './components/Form.Component';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("");
   return (
     <div className="App">
       <div className='maincontent'>
          <img src='https://i0.wp.com/www.nrtec.in/wp-content/uploads/2018/05/logo-nec1.png?w=1170&ssl=1' alt='NEC LOGO' className='clglogo' />
         <br/> 
-      <Form type="text" label="Username"/>
+      <FormComponent name={name} setName={setName}/>
       <br/>
-      <Form type="password" label="Password"/>
       <br/>
-      <Button/>
       </div>
     </div>
   );
--- src/components/Form.Component.jsx
+++ src/components/Form.Component.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import './styles.css'
-const Form= (props) => {
-    const {type, label,placeholder} = props
-  return (
-    <>
-    <div className='formdiv'>
-      <form>
-        <img src="" alt="" />
-        <label htmlFor="" className='label'>{label}</label>
-        <br />
-        <input type={type} className='inputfield' placeholder={placeholder}/>
-        </form>
-        </div>
-    </>
-  )
+import React, {useState} from "react";
+
+const FormComponent = ({name,setName }) => {
+    const [changeName, setChangeName] = useState("");
+return(
+    <div>
+        
+        <input onChange={(e)=> setChangeName(e.target.value)} />
+        <button onClick={()=> setName(changeName)}> click me for change</button>
+        <h4>{name}</h4>
+    </div>
+)
 }
 
-export default Form;
+export default FormComponent;
